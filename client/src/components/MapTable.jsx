4a07942355ed44d70b06584449dcf21674578f88
JBM <josephbinoy110@gmail.com>
2024-09-04 19:16:15 +0530

    relative path

--- client/src/components/MapTable.jsx
+++ client/src/components/MapTable.jsx
@@ -15,7 +15,7 @@ export default function MapTable( { tableType }) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/${tableType}`, {
+                const response = await axios.get(`$/api/${tableType}`, {
                     params: { pageNo, dbv: dbv }
                 });
                 const newRows = response.data.data;
