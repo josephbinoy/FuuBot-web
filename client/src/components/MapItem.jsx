1bfb21139a31683fdcbcbb036886f62d70cf0f7c
JBM <josephbinoy110@gmail.com>
2024-09-10 00:57:50 +0530

    improved hover animation

--- client/src/components/MapItem.jsx
+++ client/src/components/MapItem.jsx
@@ -9,14 +9,16 @@ export default function MapItem({mapId, mapName, mapArtist, mapper, imageUrl, ma
     }
     const borderColorClass = mapCount >= limit ? 'border-red-500' : mapCount >= limit * 0.75 ? 'border-orange-500' : 'border-green-500';    
     return (
-      <a href={`https://osu.ppy.sh/beatmapsets/${mapId}`} className="mx-auto max-w-screen-xl w-11/12 my-2">
+      <a href={`https://osu.ppy.sh/beatmapsets/${mapId}`} className="mx-auto max-w-screen-xl w-11/12 my-2 bg-">
         <div
-          className={`flex items-center justify-between bg-cover bg-center h-20 rounded-lg px-6 font-visby font-bold text-2xl text-opacity-80 shadow-osuslate-200/20 shadow-lg text-white border-r-4 ${borderColorClass} hover:-translate-y-0.5 hover:shadow-lg transition-all duration-200 hover:opacity-85`}
-          style={{
-                backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 20%, rgba(0, 0, 0, 0.4) 80%, rgba(0, 0, 0, 0.7) 100%), url(${imageUrl})`
-              }}
-            >
-            <div className="w-6/12 flex flex-col gap-1">
+          className={`relative flex items-center justify-between h-20 rounded-lg px-6 font-visby font-bold text-2xl text-opacity-80 shadow-osuslate-200/20 shadow-lg text-white border-r-4 ${borderColorClass} hover:-translate-y-0.5 hover:shadow-lg transition-all duration-200 hover:opacity-85 overflow-hidden`}>
+            <div
+                className="absolute inset-0 bg-cover bg-center transition-transform duration-500 hover:scale-105"
+                style={{
+                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 20%, rgba(0, 0, 0, 0.4) 80%, rgba(0, 0, 0, 0.7) 100%), url(${imageUrl})`,
+                }}
+            />
+            <div className="w-6/12 flex flex-col gap-1 z-10 pointer-events-none">
               <h2 className="truncate w-full">{mapName}</h2>
               <div className="text-xs flex items-center w-full">
                 <p className="truncate">{mapArtist} •<span className="text-gray-500"> Mapped by {mapper}</span></p>
